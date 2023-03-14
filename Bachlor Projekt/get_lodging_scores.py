a3b6eff59fcb8d8e15c7825d7b76348eb35c4c60
emil5654 <emil.hl@hotmail.com>
2023-03-14 13:10:18 +0100

    updateres_get_login_scores
    indeholder nu også en måde at håndtere bins på

--- Bachlor Projekt/get_lodging_scores.py
+++ Bachlor Projekt/get_lodging_scores.py
@@ -6,7 +6,14 @@ def get_labels():
     dataframe1 =  pd.read_csv('Lodging_scores.csv', parse_dates=['VisualScoreDate','FlightDate'])
     scores = np.array(dataframe1)
     labels = []
+    bin_size = 5
     #create labels, [ROI, Visual SCORE, FLIGHTFolder]
     for i in range(scores.shape[0]):
-        labels.append((scores[i,2], scores[i, 4], scores[i,6]))
+        labels.append((scores[i,2], scores[i, 4], scores[i,6], create_bin(bin_size, scores[i,4])))
     return labels
+
+def create_bin(bin_size, vision_score):
+    if (vision_score == 0):
+        return 1
+    return  np.ceil(vision_score/bin_size)
+
