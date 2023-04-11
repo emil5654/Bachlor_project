c5674e5fc8937037d86adeabced5349887cc95e5
emil5654 <emil.hl@hotmail.com>
2023-04-11 11:35:27 +0200

    Update SVM_classifier_general.py
    giveer nu dato med som feature

--- BachlorProjekt/scripts/SVM_classifier_general.py
+++ BachlorProjekt/scripts/SVM_classifier_general.py
@@ -31,23 +31,9 @@ def match_pic_label_to_names_new(features, labels, names):
         matched = False
         for j in range(m):
             if (str(labels[i][0]) == str(names[j][0]) and labels[i][2] == names[j][1]):
-                features[j].append(labels[i][3])
+                features[j].append(labels[i][4], labels[i][3])
                 matched = True
     return features
-    
-def match_pic_label_to_names_new_2(features, labels, names):
-    n = len(labels)
-    m = len(names)
-    data_frame = []
-    error = []
-    for i in range(n):
-        matched = False
-        for j in range(m):
-            if (str(labels[i][0]) == str(names[j][0]) and labels[i][2] == names[j][1]):
-                data_frame.append(j)
-                matched = True
-    return data_frame
-
 
 #error is images that has no labels
 def match_pic_names_to_label(features, labels, names):
